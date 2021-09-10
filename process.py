ea13ac6bcda40e5e75fe189d06aa3f061e27cf5d
Courtney Garner <courtneylynn108@gmail.com>
2021-09-10 15:42:03 -0600

    Python and SQL complete

--- process.py
+++ process.py
@@ -1,12 +1,24 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") #opening a server file in python
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+# def sales_reports(log_file):      #calling on the log_file function to go through data
+#     for line in log_file:         #for loop to go through the data and find needed info
+#         line = line.rstrip()      #removing any leftover characters
+#         day = line[0:3]           #calling the first three letters in  tuesday     
+#         if day == "Tue":          #day = Tuesday
+#             print(line)           #printing the result
 
 
-sales_reports(log_file)
+# sales_reports(log_file)           #calling the function
+
+
+
+def sales_reports(log_file):      #calling on the log_file function to go through data
+    for line in log_file:         #for loop to go through the data and find needed info
+        line = line.rstrip()      #removing any leftover characters
+        day = line[0:3]           #calling the first three letters on monday    
+        if day == "Mon":          #day = Monday
+            print(line)           #printing the result
+
+
+sales_reports(log_file)           #calling the function
